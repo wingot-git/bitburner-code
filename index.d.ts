9b0b089a088557301a4385566acbded78df98055
wingot-git <games@apav.me>
2023-06-22 01:21:30 +1000

    Tidying, configuring, getting Intellisense working

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,22 @@
-import { NS as OriginalNS } from "../gameSrc/src/ScriptEditor/NetscriptDefinitions";
-export * from "../gameSrc/src/ScriptEditor/NetscriptDefinitions";
+import { NS } from "NetscriptDefinitions";
+// import { NS as OriginalNS } from "gameSrc/src/ScriptEditor/NetscriptDefinitions";
+export * from "NetscriptDefinitions";
 
+declare global { const NS: NS; }
+
+/*
 interface Heart {
-  /**
-   * Get Player's current Karma
-   * @remarks
-   * RAM cost: 0 GB
-   *
-   * @returns current karma.
-   */
+   //
+   // Get Player's current Karma
+   // @remarks
+   // RAM cost: 0 GB
+   //
+   // @returns current karma.
+   //
   break(): number;
 }
+
 export interface NS extends OriginalNS {
   readonly heart: Heart;
 }
+*/
